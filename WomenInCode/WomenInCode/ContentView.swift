62eb5e168147dcfb2764c70ba0ce0be5dba0352b
Henry Calderon <theceintiant@gmail.com>
2020-11-23 15:29:18 -0800

    Project In Swift

---
+++ WomenInCode/WomenInCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WomenInCode
+//
+//  Created by Henry Calderon on 11/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
