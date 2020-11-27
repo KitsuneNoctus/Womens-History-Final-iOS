3f22c9b37bd87fbb285bbfd3fecc8f1d10c7e8da
Anika Morris <anikamorris11@gmail.com>
2020-11-27 14:05:36 -0600

    added basic about card

--- WomenInCode/WomenInCode/Views/About.swift
+++ WomenInCode/WomenInCode/Views/About.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct About: View {
+    var image: String = ""
+    var category: String = ""
+    var heading: String = "About"
+    var thesis: String = "It is time to look into the past of the women who code and learn from the industry’s previous mistakes to create a better future for those here now."
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.secondary)
+                        .lineLimit(3)
+                    Text(thesis)
+                        .font(.title)
+                        .foregroundColor(.primary)
+                        .padding([.top])
+
+                }
+                .layoutPriority(100)
+         
+                Spacer()
+            }
+            .padding()
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
+                .background(Color(.sRGB, red: 190/255, green: 190/255, blue: 250/255, opacity: 0.5))
+        )
+        .cornerRadius(10)
+        .padding([.top, .horizontal])
+        
     }
 }
 
