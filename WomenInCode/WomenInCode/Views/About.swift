3d4be049f89a6dd3c7f7a324ca14564219045b68
Anika Morris <anikamorris11@gmail.com>
2020-11-27 14:11:50 -0600

    generalized about card to CardView

--- WomenInCode/WomenInCode/Views/About.swift
+++ WomenInCode/WomenInCode/Views/About.swift
@@ -12,39 +12,10 @@ struct About: View {
     var category: String = ""
     var heading: String = "About"
     var thesis: String = "It is time to look into the past of the women who code and learn from the industry’s previous mistakes to create a better future for those here now."
+    var bodyColor = Color(red: 190/255, green: 190/255, blue: 250/255, opacity: 0.5)
     
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(category)
-                        .font(.headline)
-                        .foregroundColor(.secondary)
-                    Text(heading)
-                        .font(.title)
-                        .fontWeight(.black)
-                        .foregroundColor(.secondary)
-                        .lineLimit(3)
-                    Text(thesis)
-                        .font(.title)
-                        .foregroundColor(.primary)
-                        .padding([.top])
-
-                }
-                .layoutPriority(100)
-         
-                Spacer()
-            }
-            .padding()
-        }
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
-                .background(Color(.sRGB, red: 190/255, green: 190/255, blue: 250/255, opacity: 0.5))
-        )
-        .cornerRadius(10)
-        .padding([.top, .horizontal])
-        
+        CardView(image: nil, category: category, heading: heading, bodyText: thesis, bodyColor: bodyColor)
     }
 }
 
---
+++ WomenInCode/WomenInCode/Views/CardView.swift
@@ -0,0 +1,62 @@
+//
+//  CardView.swift
+//  WomenInCode
+//
+//  Created by Anika Morris on 11/27/20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var image: String?
+    var category: String = ""
+    var heading: String = ""
+    var bodyText: String = ""
+    var bodyColor: Color = Color(.gray)
+    
+    var body: some View {
+        VStack {
+            if let image = image {
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.secondary)
+                        .lineLimit(3)
+                    Text(bodyText)
+                        .font(.title)
+                        .foregroundColor(.primary)
+                        .padding([.top])
+
+                }
+                .layoutPriority(100)
+         
+                Spacer()
+            }
+            .padding()
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.2), lineWidth: 1)
+                .background(bodyColor)
+        )
+        .cornerRadius(10)
+        .padding([.top, .horizontal])
+        
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
