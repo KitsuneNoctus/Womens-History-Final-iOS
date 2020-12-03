cd7028389e92f3e4f6acb0d0e47769f8f919348d
Anika Morris <anikamorris11@gmail.com>
2020-12-02 21:30:48 -0600

    updated challenges view

---
+++ WomenInCode/WomenInCode/Views/ChallengesFlow/UnequalOpportunities.swift
@@ -0,0 +1,20 @@
+//
+//  UnequalOpportunities.swift
+//  WomenInCode
+//
+//  Created by Anika Morris on 12/2/20.
+//
+
+import SwiftUI
+
+struct UnequalOpportunities: View {
+    var body: some View {
+        Text("Unequal Opportunities")
+    }
+}
+
+struct UnequalOpportunities_Previews: PreviewProvider {
+    static var previews: some View {
+        UnequalOpportunities()
+    }
+}
